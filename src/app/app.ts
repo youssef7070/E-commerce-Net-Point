426ae0918218041cef9a48e2c18376fcb9b06b32
asmaamaged2022 <asmaamaged96@gmail.com>
2026-07-22 18:15:42 +0300

    nav bar

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MainNavbarComponent } from "./Features/main-navbar/main-navbar.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MainNavbarComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })

8418e4b747be573df5778bed610c8f90b18e3660
Maryam Mostafa <maryammostafaawad22@gmail.com>
2026-07-27 12:03:55 +0300

    Add categories feature

--- src/app/Core/Services/categories.service.ts
+++ src/app/Core/Services/categories.service.ts
@@ -1,8 +1,13 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
+import { BaseURL } from '../../environment/environment.local';
+import { Observable } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class CategoriesService {
     private _httpClient = inject(HttpClient);
 
+    getAllCategories():Observable<any>{
+        return this._httpClient.get(`${BaseURL}/api/v1/categories`);
+    }
 }
--- src/app/Features/categories/categories.component.ts
+++ src/app/Features/categories/categories.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { CategoriesService } from '../../core/Services/categories.service';
+import { Category } from '../../core/Interfaces/category.interface';
 
 @Component({
   selector: 'app-categories',
@@ -6,4 +8,17 @@ import { Component } from '@angular/core';
   templateUrl: './categories.component.html',
   styleUrl: './categories.component.scss',
 })
-export class CategoriesComponent {}
+export class CategoriesComponent {
+  private categoriesService = inject(CategoriesService);
+  categories = signal<Category[]>([]);
+
+  getAllCategories() {
+    this.categoriesService.getAllCategories().subscribe((response: any) => {
+      this.categories.set(response.data ?? []);
+    });
+  }
+
+  ngOnInit() {
+    this.getAllCategories();
+  }
+}

aa0838c9d6d13c25bc1eec21eb0e82c802e0a0ea
Radwa Mohamed <radwasedeek2013@gmail.com>
2026-08-12 17:32:02 +0300

    create translation service and language files

---
+++ src/app/Core/Services/my-translation.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import {TranslateService} from "@ngx-translate/core";
+
+@Injectable({
+    providedIn: 'root'
+})
+
+export class MyTranslationService {
+
+    constructor(private _TranslateService: TranslateService) {
+    const savedLang = localStorage.getItem('lang') || 'en';
+
+    this._TranslateService.use(savedLang);
+    this.changeDirection();
+  }
+
+  changeDirection() {
+    const savedLang = localStorage.getItem('lang') || 'en';
+
+    if (savedLang === 'en') {
+      document.documentElement.dir = 'ltr';
+      document.documentElement.lang = 'en';
+    } else if (savedLang === 'ar') {
+      document.documentElement.dir = 'rtl';
+      document.documentElement.lang = 'ar';
+    }
+  }
+
+  changeLang(lang: string) {
+    this._TranslateService.use(lang);
+    localStorage.setItem('lang', lang);
+    this.changeDirection();
+  }
+}
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -8,12 +8,24 @@ import { headerInterceptor } from './core/Interceptors/header.interceptor';
 import { globalErrorInterceptor } from './core/Interceptors/global-error.interceptor';
 import { NgxSpinnerModule } from 'ngx-spinner';
 import { loadingInterceptor } from './core/Interceptors/loading.interceptor';
+import {provideTranslateService, TranslateService} from "@ngx-translate/core";
+import {provideTranslateHttpLoader} from "@ngx-translate/http-loader";
+
+
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes, withComponentInputBinding(), withHashLocation()),
     provideHttpClient(withFetch(),withInterceptors([headerInterceptor,globalErrorInterceptor,loadingInterceptor])),
+    provideTranslateService({
+      lang: 'en',
+      fallbackLang: 'ar',
+      loader: provideTranslateHttpLoader({
+        prefix: '/i18n/',
+        suffix: '.json'
+      })
+    }),
     provideToastr(),
     NgxSpinnerModule
   ]
